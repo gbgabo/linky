7bb58e5905720f7304263be5180431e1385e61e7
gabo <gabriel.gaboardi13@gmail.com>
2022-04-06 19:51:31 -0300

    simplified customize section

--- client/src/components/Customize.tsx
+++ client/src/components/Customize.tsx
@@ -55,11 +55,81 @@ export default function Customize({
   onChange,
 }: CustomizeProps): ReactElement {
   const classes = useStyles();
-  const [expandedMenu, setExpandedMenu] = React.useState(0);
+  const [expandedMenu, setExpandedMenu] = React.useState(-1);
 
-  const handleChange =
+  const menuList = [
+    {
+      title: "Buttons",
+      icon: <Storage />,
+      details: <ButtonStyler button={styles.button} onChange={handleStyle} />,
+    },
+    {
+      title: "Background",
+      icon: <CropOriginal />,
+      details: <BackgroundStyler page={styles.page} onChange={handleStyle} />,
+    },
+    {
+      title: "Profile",
+      icon: <Person />,
+      details: (
+        <ProfileStyler profile={styles.profile} onChange={handleStyle} />
+      ),
+    },
+    {
+      title: "Section",
+      icon: <Info />,
+      details: (
+        <SectionStyler section={styles.section} onChange={handleStyle} />
+      ),
+    },
+    {
+      title: "Panel",
+      icon: <Storage />,
+      details: <PanelStyler panel={styles.panel} onChange={handleStyle} />,
+    },
+    {
+      title: "Themes",
+      icon: <Palette />,
+      details: (
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          spacing={1}
+        >
+          <Grid item>
+            <Button
+              style={gradientButton as CSSProperties}
+              onClick={() => handleStyle("button", gradientButton)}
+            >
+              Mermaid
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              style={flatButton as CSSProperties}
+              onClick={() => handleStyle("button", flatButton)}
+            >
+              Flat
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              style={materialButton as CSSProperties}
+              onClick={() => handleStyle("button", materialButton)}
+            >
+              Material
+            </Button>
+          </Grid>
+        </Grid>
+      ),
+    },
+  ];
+
+  const handleAccordion =
     (panel: number) => (event: ChangeEvent<{}>, isExpanded: boolean) => {
-      setExpandedMenu(isExpanded ? panel : 0);
+      setExpandedMenu(isExpanded ? panel : -1);
     };
 
   function handleStyle(selector: string, rules = {}) {
@@ -72,131 +142,24 @@ export default function Customize({
 
   return (
     <div className={classes.root}>
-      <Accordion expanded={expandedMenu === 1} onChange={handleChange(1)}>
-        <AccordionSummary
-          expandIcon={<ExpandMore />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>
-            <Storage />
-          </Typography>
-          <Typography className={classes.heading}>Buttons</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <ButtonStyler button={styles.button} onChange={handleStyle} />
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion expanded={expandedMenu === 2} onChange={handleChange(2)}>
-        <AccordionSummary
-          expandIcon={<ExpandMore />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography className={classes.heading}>
-            <CropOriginal />
-          </Typography>
-          <Typography className={classes.heading}>Background</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <BackgroundStyler page={styles.page} onChange={handleStyle} />
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion expanded={expandedMenu === 3} onChange={handleChange(3)}>
-        <AccordionSummary
-          expandIcon={<ExpandMore />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography className={classes.heading}>
-            <Person />
-          </Typography>
-          <Typography className={classes.heading}>Profile</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <ProfileStyler profile={styles.profile} onChange={handleStyle} />
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion expanded={expandedMenu === 4} onChange={handleChange(4)}>
-        <AccordionSummary
-          expandIcon={<ExpandMore />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography className={classes.heading}>
-            <Info />
-          </Typography>
-          <Typography className={classes.heading}>Section</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <SectionStyler section={styles.section} onChange={handleStyle} />
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion expanded={expandedMenu === 5} onChange={handleChange(5)}>
-        <AccordionSummary
-          expandIcon={<ExpandMore />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>
-            <Storage />
-          </Typography>
-          <Typography className={classes.heading}>Panel</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <PanelStyler panel={styles.panel} onChange={handleStyle} />
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expandedMenu === 6} onChange={handleChange(6)}>
-        <AccordionSummary
-          expandIcon={<ExpandMore />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography className={classes.heading}>
-            <Palette />
-          </Typography>
-          <Typography className={classes.heading}>Themes</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-            spacing={1}
+      {menuList.map((menu, index) => {
+        return (
+          <Accordion
+            expanded={expandedMenu === index}
+            onChange={handleAccordion(index)}
           >
-            <Grid item>
-              <Button
-                style={gradientButton as CSSProperties}
-                onClick={() => handleStyle("button", gradientButton)}
-              >
-                Sereia
-              </Button>
-            </Grid>
-            <Grid item>
-              <Button
-                style={flatButton as CSSProperties}
-                onClick={() => handleStyle("button", flatButton)}
-              >
-                Flat
-              </Button>
-            </Grid>
-            <Grid item>
-              <Button
-                style={materialButton as CSSProperties}
-                onClick={() => handleStyle("button", materialButton)}
-              >
-                Material
-              </Button>
-            </Grid>
-          </Grid>
-        </AccordionDetails>
-      </Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography className={classes.heading}>{menu.icon}</Typography>
+              <Typography className={classes.heading}>{menu.title}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>{menu.details}</AccordionDetails>
+          </Accordion>
+        );
+      })}
     </div>
   );
 }
